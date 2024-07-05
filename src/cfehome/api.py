05eaf18766230b2cd01b93da340de7b99b272a53
Tiny P Christhudas <tinypchristhudas@gmail.com>
2024-07-05 09:21:51 +0530

    jwt authentication with ninja

---
+++ src/cfehome/api.py
@@ -0,0 +1,26 @@
+from ninja import NinjaAPI, Schema
+from ninja_jwt.controller import NinjaJWTDefaultController
+from ninja_jwt.authentication import JWTAuth
+from ninja_extra import NinjaExtraAPI
+
+# schema for defining how i want to convert my user object into json data 
+
+api = NinjaExtraAPI()
+api.register_controllers(NinjaJWTDefaultController)
+
+
+class UserSchema(Schema):
+    username: str
+    is_authenticated: bool
+    email: str = None
+
+
+@api.get("/hello")
+def hello(request):
+    print(request)
+    return "Hello, world!"
+
+@api.get("/me", response=UserSchema, auth=JWTAuth())
+def me(request):
+    print(request)
+    return request.user
